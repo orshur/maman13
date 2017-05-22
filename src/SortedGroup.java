bf366386bb6ef2e3c7268517102d02f299f3974c
or <or.shur@outlook.com>
2017-05-22 22:41:08 +0300

    sortedgroup partial

--- src/SortedGroup.java
+++ src/SortedGroup.java
@@ -1,8 +1,32 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 /**
  * Created by or on 22/05/17.
  */
-public class SortedGroup
+public class SortedGroup<T extends Comparable<T>>
 {
+    private ArrayList<T> _elements;
+
+    public SortedGroup()
+    {
+        _elements = new ArrayList<T>();
+    }
+
+    void Add(T element)
+    {
+        if (_elements.size() == 0)
+        {
+            _elements.add(element);
+            return;
+        }
+
+        int insertInd = 0;
+        while (element.compareTo(_elements.get(insertInd)) < 0)
+        {
+            insertInd++;
+        }
+
+        _elements.add(insertInd, element);
+    }
+
 }
